7140958a80cd56fca0150b57ff700478e72e1442
Jonas <schmeisser.jonas@icloud.com>
2026-05-16 15:16:34 +0200

    feat: rate limiting and 404 custom response

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import Fastify from 'fastify'
 import infoRoutes from "./routes/about.js"
+import rateLimit from "@fastify/rate-limit"
 
 const fastify = Fastify({
   logger: true
 })
 
-fastify.register(infoRoutes) // Register imported routes
+await fastify.register(rateLimit, {
+  max: 100,
+  timeWindow: "1 minute"
+})
+
+await fastify.register(infoRoutes)
 
 // Run the server!
 try {
+  fastify.setNotFoundHandler((request, reply) => {
+    reply.status(404).send({
+      statusCode: 404,
+      error: "Not Found",
+      message: `Route ${request.method} ${request.url} not found. Available routes: GET /about, GET /skills, GET /projects, GET /tools, GET /experience, GET /interests, GET /health or read the README of the github repo for more info.`
+    })
+  })
   await fastify.listen({ port: 3000 })
 } catch (err) {
   fastify.log.error(err)
